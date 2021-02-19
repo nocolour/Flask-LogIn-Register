c985de78a03ea0c19b16c0b5e0898822f5e066c5
Desmond Phee <nocolour@gmail.com>
2021-02-19 15:40:41 +0800

    initial commit

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '!9m@S-dThyIlW[pHQbN^'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/auth'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'  # 'mysql://root:root@localhost/auth'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
 
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    name= db.Column(db.String(15), unique=True)
+    name = db.Column(db.String(15), unique=True)
 
     username = db.Column(db.String(15), unique=True)
 
@@ -28,29 +28,29 @@ class User(db.Model):
 
     password = db.Column(db.String(256), unique=True)
 
+
 @app.route('/')
 def home():
-
     return render_template('index.html')
 
-@app.route('/login/', methods = ['GET', 'POST'])
-def login():
 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
     form = LoginForm(request.form)
 
     if request.method == 'POST' and form.validate:
 
-        user = User.query.filter_by(email = form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
 
         if user:
 
             if check_password_hash(user.password, form.password.data):
 
                 flash('You have successfully logged in.', "success")
-                
+
                 session['logged_in'] = True
 
-                session['email'] = user.email 
+                session['email'] = user.email
 
                 session['username'] = user.username
 
@@ -62,49 +62,48 @@ def login():
 
                 return redirect(url_for('login'))
 
-    return render_template('login.html', form = form)
+    return render_template('login.html', form=form)
 
 
-@app.route('/register/', methods = ['GET', 'POST'])
+@app.route('/register/', methods=['GET', 'POST'])
 def register():
-    
     form = RegisterForm(request.form)
-    
+
     if request.method == 'POST' and form.validate():
-    
+
         hashed_password = generate_password_hash(form.password.data, method='sha256')
-    
+
         new_user = User(
-            
-            name = form.name.data, 
-            
-            username = form.username.data, 
-            
-            email = form.email.data, 
-            
-            password = hashed_password)
-    
+
+            name=form.name.data,
+
+            username=form.username.data,
+
+            email=form.email.data,
+
+            password=hashed_password)
+
         db.session.add(new_user)
-    
+
         db.session.commit()
-    
+
         flash('You have successfully registered', 'success')
-    
+
         return redirect(url_for('login'))
-    
+
     else:
-    
-        return render_template('register.html', form = form)
+
+        return render_template('register.html', form=form)
+
 
 @app.route('/logout/')
 def logout():
-    
     session['logged_in'] = False
 
     return redirect(url_for('home'))
 
+
 if __name__ == '__main__':
-    
     db.create_all()
-    
+
     app.run(debug=True)
